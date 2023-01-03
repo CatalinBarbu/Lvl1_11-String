b2f45fd7e2c1301ffb752d50a70305828e1c390b
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-03 13:20:23 +0100

    First commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,15 @@
+let x = "Hello ";
+let y = "World<br>";
+z = x + y;
+document.write(z);
+
+n = x + " " + y;
+document.write(n);
+
+x = "World";
+document.write(x);
+
+const meinString = "Ich bin Erster.";
+
+string = meinString + " Ich komme auf Platz zwei";
+console.log(string);
